92388325ef2324160a57efa856fd3c98515009bd
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-10-17 16:53:00 -0500

    Inital commit.

---
+++ js/util.js
@@ -0,0 +1,14 @@
+function ParseObject(obj) {
+    Object.keys(obj).forEach(function (key) {
+        
+        if( obj === undefined)
+            return;
+
+        if( typeof obj[key] == 'string' )
+            ;
+        
+        if( typeof obj[key] == '')
+            ;
+
+    });
+}

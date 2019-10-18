5a2e8a9529f178c669351cfa7bac3ac651dd38b7
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-10-18 12:42:53 -0500

    Got the basic sections to render
    This code is a mess, I will need to set up a web server to feed the page the json file.

--- js/util.js
+++ js/util.js
@@ -1,14 +1,50 @@
-function ParseObject(obj) {
-    Object.keys(obj).forEach(function (key) {
-        
-        if( obj === undefined)
-            return;
+function RenderData(me, layer = 1) {
 
-        if( typeof obj[key] == 'string' )
-            ;
-        
-        if( typeof obj[key] == '')
-            ;
+    if (me === undefined)
+        return '';
 
-    });
+    if (layer > 5)
+        layer = 5;
+
+    if (typeof me === 'string' || typeof me === 'number')
+        return '<p>' + me + '</p>';
+
+    if (Array.isArray(me)) {
+        let message = ''
+        me.forEach(function (o) {
+            message += RenderData(o, layer);
+        });
+        return message;
+    }
+
+    if (typeof me !== 'object' || me['type'] === undefined)
+        return '';
+
+    if (me['type'] === 'section' || me['type'] === 'entries') {
+        if (me['name'] === undefined && me['entries'] === undefined) {
+            return '';
+        }
+
+        let message = '<div>';
+
+        if (me['name'] !== undefined) {
+            message += '<h' + layer + '>' + me['name'] + '</h' + layer + '>';
+        }
+
+        if (me['entries'] !== undefined) {
+            me['entries'].forEach(function (entry) {
+                message += RenderData(entry, layer + 1);
+            });
+        }
+
+        message += "</div>";
+
+        return message;
+    }
+
+    //TODO: render tables.
+    //TODO: render lists.
+    //TODO: render images.
+
+    return '';
 }
